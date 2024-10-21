a202db1efc55c2cad8acf7d41986d533f7b4ea73
noahmvc <nas00001@mix.wvu.edu>
2024-10-21 15:54:52 -0400

    first commit

---
+++ 4-streamlit.py
@@ -0,0 +1,8 @@
+#streamlit run 4-streamlit.py
+import streamlit as st
+
+st.title('My first Streamlit app')
+st.subheader('Introducting Streamlit, by Noah')
+st.write(f'''Hello guys, today I will be showcasing my first Streamlit web app
+         
+         Hope you enjoy!''')
