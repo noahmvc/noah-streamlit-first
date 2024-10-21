3ee14fa21863d0bf6c56204d0a4cd03d837ee57e
noahmvc <nas00001@mix.wvu.edu>
2024-10-21 16:04:21 -0400

    second commit

--- 4-streamlit.py
+++ 4-streamlit.py
@@ -1,8 +1,18 @@
 #streamlit run 4-streamlit.py
 import streamlit as st
+import pandas
+
+data = {
+    'Series_1': [1,3,6,7,10],
+    'Series_2' : [10,30,40,50,70]
+}
+
+df = pandas.DataFrame(data)
 
 st.title('My first Streamlit app')
 st.subheader('Introducting Streamlit, by Noah')
 st.write(f'''Hello guys, today I will be showcasing my first Streamlit web app
          
          Hope you enjoy!''')
+
+st.write(df)
